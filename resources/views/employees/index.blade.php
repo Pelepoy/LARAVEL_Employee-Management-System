351a5d2fb615182a6436340f22b8a45c522c51cc
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2023-11-17 14:36:47 +0800

    Remove the session success message

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,22 +1,14 @@
 @extends('layouts.app')
 @section('content')
-
-<div class="px-5">
-    @if (Session::has('success'))
-        <div class="alert alert-success">
-            {{ Session::get('success') }}
-        </div>
-    @endif
-</div>
     <div class="p-5">
         <div class='float-end mb-3'>
             <a href="{{ route('employee.create') }}" class='btn btn-primary shadow'>+Add new employee</a>
-                <form action="{{ route('user.logout')}}" method="POST">
-                    @csrf
+            <form action="{{ route('user.logout') }}" method="POST" class='d-inline-block'>
+                @csrf
                 <button class="btn btn-warning">Logout</button>
             </form>
         </div>
-        <h1 class="fs-1 text-center mb-5">DARK CONTINENT EMPLOYEES</h1>
+        <h1 class="fs-1 text-center mb-5">EMPLOYEES LIST</h1>
         <table class="table table-bordered shadow">
             <thead>
                 <tr>
@@ -31,18 +23,19 @@
             <tbody>
                 @forelse ($employees as $employee)
                     <tr class='text-capitalize'>
-                        <td>{{ $employee->employee_id}}</td>
+                        <td>{{ $employee->employee_id }}</td>
                         <td>{{ $employee->firstname }}</td>
                         <td>{{ $employee->middlename }}</td>
                         <td>{{ $employee->lastname }}</td>
                         <td>{{ $employee->suffix }}</td>
                         <td class="text-center">
-                            <a href="{{ route('employee.edit', $employee->employee_id) }}" class="btn btn-success">EDIT</a> 
+                            <a href="{{ route('employee.edit', $employee->employee_id) }}" class="btn btn-success">EDIT</a>
                             <a href="{{ route('employee.show', $employee->employee_id) }}" class="btn btn-info">SHOW</a>
-                            <form action="{{ route('employee.destroy', $employee->employee_id) }}" method="POST" class="d-inline-block">
+                            <form action="{{ route('employee.destroy', $employee->employee_id) }}" method="POST"
+                                class="d-inline-block">
                                 @csrf
                                 @method('DELETE')
-                            <button type="submit" class="btn btn-danger">DELETE</button>
+                                <button type="submit" class="btn btn-danger">DELETE</button>
                             </form>
                         </td>
                     </tr>
@@ -53,6 +46,5 @@
                 @endforelse
             </tbody>
         </table>
-
     </div>
 @endsection
