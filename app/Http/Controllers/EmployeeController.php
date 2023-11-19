9cdb0b2169c4ec3dc289e49844d251555c1f0303
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2023-11-19 09:35:52 +0800

    Add title

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -11,109 +11,111 @@ use function Ramsey\Uuid\v1;
 class EmployeeController extends Controller
 {
 
-    public function index()
-    {
+	public function index()
+	{
 
-        $employees = Employee::orderBy('created_at', 'desc')->get();
+		$title = 'Sic Mundus | Employees List';
+		$employees = Employee::orderBy('created_at', 'desc')->get();
 
-        return view('employees.index', [
-            'employees' => $employees,
-        ]);
-    }
+		return view('employees.index', [
+			'employees' => $employees,
+			'title' => $title,
+		]);
+	}
+
+	public function create()
+	{
+
+		$title = 'Sic Mundus | Create Employee';
+		$suffix = Employee::SUFFIX;
+
+		return view('employees.create', [
+			'suffix' => $suffix,
+			'title' => $title,
+		]);
+	}
+
+	public function store(Request $request)
+	{
+
+		try {
+			$suffix = Employee::SUFFIX;
+
+			$validated = $request->validate([
+				'firstname' => ['required', 'max:50'],
+				'middlename' => ['nullable', 'min:2', 'max:50'],
+				'lastname' => ['required', 'max:50'],
+				'suffix' => ['nullable', 'in:' . implode(',', $suffix)],
+			]);
+
+			$employee = Employee::create($validated);
+			$employee->save();
+
+			return redirect()->route('employee.index')->with('success', 'Employee created successfully.');
+		} catch (\Exception $e) {
+			dd($e->getMessage());
+			return back()->withInput()->withErrors(['error' => 'An error occurred while creating the employee.']);
+		}
+	}
+
+	public function show($employeeID)
+	{
+		$title = 'Sic Mundus | Employee Details';
+		$employee = Employee::findOrFail($employeeID);
+
+		return view('employees.show', [
+			'employee' => $employee,
+			'title' => $title,
+
+		]);
+	}
+
+	public function edit($employeeID)
+	{
+
+		$title = 'Sic Mundus | Edit Employee';
+		$suffix = Employee::SUFFIX;
+
+		$employee = Employee::find($employeeID);
+
+		return view('employees.edit', [
+			'employee' => $employee,
+			'suffix' => $suffix,
+			'title' => $title,
+		]);
+	}
 
 
-    public function create()
-    {
-        $suffix = Employee::SUFFIX;
+	public function update(Request $request, $employeeID)
+	{
+		try {
+			$suffix = Employee::SUFFIX;
 
-        return view('employees.create', [
-            'suffix' => $suffix,
-        ]);
-    }
+			$validated = $request->validate([
+				'firstname' => ['required', 'max:50'],
+				'middlename' => ['nullable', 'min:2', 'max:50'],
+				'lastname' => ['required', 'max:50'],
+				'suffix' => ['nullable', 'in:' . implode(',', $suffix)],
+			]);
 
-    public function store(Request $request)
-    {
-        // dd($request->all());
+			Employee::find($employeeID)->update($validated);;
 
-        try {
-            $suffix = Employee::SUFFIX;
+			return redirect()->route('employee.index')->with('success', 'Employee updated successfully.');
+		} catch (\Exception $e) {
+			dd($e->getMessage());
+			return back()->withInput()->withErrors(['error' => 'An error occurred while updating the employee.']);
+		}
+	}
 
-            $validated = $request->validate([
-                // 'employee_id' => ['required', 'unique:employees,employee_id', 'max:6'],
-                'firstname' => ['required', 'max:50'],
-                'middlename' => ['nullable', 'min:2', 'max:50'],
-                'lastname' => ['required', 'max:50'],
-                'suffix' => ['nullable', 'in:' . implode(',', $suffix)],
-            ]);
+	public function destroy($employeeID)
+	{
+		try {
+			Employee::find($employeeID)->delete();
 
-            $employee = Employee::create($validated);
-            $employee->save();
-
-            return redirect()->route('employee.index')->with('success', 'Employee created successfully.');
-        } catch (\Exception $e) {
-            dd($e->getMessage());
-            return back()->withInput()->withErrors(['error' => 'An error occurred while creating the employee.']);
-        }
-    }
-
-
-
-    public function show($employeeID)
-    {
-        // dd('You are in show method');
-
-
-        $employee = Employee::findOrFail($employeeID);
-        return view('employees.show', [
-            'employee' => $employee,
-
-        ]);
-    }
-
-
-    public function edit($employeeID)
-    {
-        $suffix = Employee::SUFFIX;
-
-        $employee = Employee::find($employeeID);
-
-        return view('employees.edit', [
-            'employee' => $employee,
-            'suffix' => $suffix,
-        ]);
-    }
-
-
-    public function update(Request $request, $employeeID)
-    {
-        try {
-            $suffix = Employee::SUFFIX;
-
-            $validated = $request->validate([
-                'firstname' => ['required', 'max:50'],
-                'middlename' => ['nullable', 'min:2', 'max:50'],
-                'lastname' => ['required', 'max:50'],
-                'suffix' => ['nullable', 'in:' . implode(',', $suffix)],
-            ]);
-
-            Employee::find($employeeID)->update($validated);;
-
-            return redirect()->route('employee.index')->with('success', 'Employee updated successfully.');
-        } catch (\Exception $e) {
-            dd($e->getMessage());
-            return back()->withInput()->withErrors(['error' => 'An error occurred while updating the employee.']);
-        }
-    }
-
-    public function destroy($employeeID)
-    {
-        try {
-            Employee::find($employeeID)->delete();
-
-            return back()->with('success', 'Employee deleted successfully.');
-        } catch (\Exception $e) {
-            dd($e->getMessage());
-            return back()->withInput()->withErrors(['error' => 'An error occurred while deleting the employee.']);
-        }
-    }
+			return back()->with('success', 'Employee deleted successfully.');
+		} catch (\Exception $e) {
+			dd($e->getMessage());
+			return back()->withInput()->withErrors(['error' => 'An error occurred while deleting the employee.']);
+		}
+	}
 }
